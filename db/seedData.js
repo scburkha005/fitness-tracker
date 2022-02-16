2545ea1c2d142831969420f163c580b9edbc6fab
scburkha005 <scburkha005@gmail.com>
2022-02-16 15:51:44 -0800

    drop/create activities table

--- db/seedData.js
+++ db/seedData.js
@@ -5,12 +5,22 @@ const client = require('./client');
 async function dropTables() {
   console.log('Dropping All Tables...');
   // drop all tables, in the correct order
+  await client.query(`
+    DROP TABLE IF EXISTS activities; 
+  `)
 
 }
 
 async function createTables() {
   console.log("Starting to build tables...");
   // create all tables, in the correct order
+  await client.query(`
+    CREATE TABLE activities(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+    );
+  `)
 
 }
 
