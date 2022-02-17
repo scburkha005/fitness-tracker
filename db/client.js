8e523f80ff2580dfb54901db620dbe956f3eae6e
Brian Hogue <bhogue32@gmail.com>
2022-02-16 19:28:22 -0800

    Created and dropped users tables, fixed psql port

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 // build and export your unconnected client here
 const { Client } = require('pg')
-const CONNECTIONSTRING = process.env.DATABASE_URL || 'postgres://localhost:5433/fitness-dev'
+const CONNECTIONSTRING = process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev'
 const client = new Client(CONNECTIONSTRING)
 
 module.exports = client
