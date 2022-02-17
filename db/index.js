387e0ab7686d28744f1bea62ce21e78303128843
Brian Hogue <bhogue32@gmail.com>
2022-02-16 20:15:38 -0800

    Exported/Imported createUser

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 // require and re-export all files in this db directory (users, activities...)
 const { createActivity } = require('./activities');
+const { createUser } = require('./users');
 
 module.exports = {
-  createActivity
+  createActivity,
+  createUser
 }
