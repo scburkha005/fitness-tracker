f31631dfa0d68c19d30ef2ba7fec39c4841e4018
scburkha005 <scburkha005@gmail.com>
2022-02-18 18:22:13 -0800

    init routines.js, createRoutine

---
+++ db/routines.js
@@ -0,0 +1,13 @@
+const client = require('./client');
+
+const createRoutine = async ({ creatorId, isPublic, name, goal }) => {
+  try {
+
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  createRoutine
+}
