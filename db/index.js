c3e2495389422e3ef04177551c1d9c0903b587b7
scburkha005 <scburkha005@gmail.com>
2022-02-19 11:48:26 -0800

    properly formatted reexport file

--- db/index.js
+++ db/index.js
@@ -1,19 +1,7 @@
 // require and re-export all files in this db directory (users, activities...)
-const { createActivity, getAllActivities, getActivityById, updateActivity } = require('./activities');
-const { createUser, getUser } = require('./users');
-const { createRoutine, getRoutinesWithoutActivities, getAllRoutines, getRoutineById } = require('./routines')
-const { addActivityToRoutine } = require('./routine_activities');
-
 module.exports = {
-  createActivity,
-  createUser,
-  getAllActivities,
-  getUser,
-  getActivityById,
-  updateActivity,
-  createRoutine,
-  getRoutinesWithoutActivities,
-  getAllRoutines,
-  addActivityToRoutine,
-  getRoutineById
+  ...require('./users'),
+  ...require('./activities'),
+  ...require('./routines'),
+  ...require('./routine_activities')
 }
