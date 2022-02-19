a59768bac2185e5f00815679e679a3eb2e85df2d
scburkha005 <scburkha005@gmail.com>
2022-02-18 18:30:34 -0800

    createRoutine returns single routine

--- db/routines.js
+++ db/routines.js
@@ -2,7 +2,13 @@ const client = require('./client');
 
 const createRoutine = async ({ creatorId, isPublic, name, goal }) => {
   try {
+    const { rows: [routine] } = await client.query(`
+      INSERT INTO routines("creatorId", "isPublic", name, goal)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [creatorId, isPublic, name, goal]);
 
+    return routine;
   } catch (err) {
     throw err;
   }
