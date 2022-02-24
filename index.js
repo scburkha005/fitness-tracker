1f1e7ff640b4fb67fcf524e75ec95c337aeba2c4
scburkha005 <scburkha005@gmail.com>
2022-02-23 21:35:07 -0800

    init express server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // create the express server here
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const client = require('./db/client');
+
+const { PORT = 3000 } = process.env;
+
+app.listen(3000, () => {
+  client.connect();
+  console.log(`The server is listening on http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res, next) => {
+  res.send("Welcome to the homepage");
+})
