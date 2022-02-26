f58dc983b76c4d8b75d19814adf3edd162d73a18
scburkha005 <scburkha005@gmail.com>
2022-02-26 14:21:31 -0800

    requireUserOwner still needs completion

--- api/utils.js
+++ api/utils.js
@@ -9,13 +9,15 @@ const requireUser = (req, res, next) => {
   next();
 }
 
-// const requireActiveUser = (req, res, next) => {
+// const requireUserOwner = (req, res, next) => {
 //   if (!req.user) {
 //     next({
 //       name: "MissingUserError",
 //       message: "You must be logged in to perform this action"
 //     });
-//   } else if (!req.user.active)
+//   } else if () {
+
+//   }
 // }
 
 module.exports = {
