1e766b6271da14b18a87023eca34ae1bbb9051fa
scburkha005 <scburkha005@gmail.com>
2022-02-26 14:08:54 -0800

    auth to add the user object to the req obj if the jwt token is correct

--- api/index.js
+++ api/index.js
@@ -1,8 +1,50 @@
 // create an api router
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
- const express = require('express');
- const router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+
+const { JWT_SECRET } = process.env;
+
+
+//JWT Auth
+router.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+  
+  if (!auth) {
+    next();
+  }
+  else if (auth.startsWith(prefix)) {
+    const [ , token] = auth.split(' ');
+    try {
+     const { id } = jwt.verify(token, JWT_SECRET);
+
+     if (id) {
+       req.user = await getUserById(id);
+       next();
+     }
+    } catch ({ name, message }) {
+      next({ name, message })
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`
+    });
+  }
+});
+
+//log user
+router.use((req, res, next) => {
+  if (req.user) {
+    console.log('User is set:', req.user);
+  }
+  
+  next();
+});
 
 //Create /api calls and router.use('/otherapifile', require('./otherapifile)) here
 // api/activities
